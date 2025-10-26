18cfbe4e82fff9dd7b0d4774ee7e38dc5e2a7f5a
Lindamust <208328889+Lindamust@users.noreply.github.com>
2025-10-26 12:59:05 +1100

    builder method for constrction

--- src/vocab_importer.rs
+++ src/vocab_importer.rs
@@ -12,12 +12,25 @@ pub struct JapaneseVocabImporter {
 
 impl JapaneseVocabImporter {
     
+    /// create a new importer with default settings
     pub fn new(deck_name: impl Into<String>) -> Self {
         JapaneseVocabImporter {
             client: AnkiConnectClient::new(),
             deck_name: deck_name.into(),
             model_name: "Basic".to_string()  // <--- will add support for other models later
         }
+    }
+
+    /// Set a custom note type/model
+    pub fn with_model(mut self, model_name: impl Into<String>) -> Self {
+        self.model_name = model_name.into();
+        self
+    }
+
+    
+    pub fn with_url(mut self, url: impl Into<String>) -> Self {
+        self.client = AnkiConnectClient::with_url(url);
+        self
     }
 
 
